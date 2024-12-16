6f735616a522fce4c7e8a7ddcafde669cb7b9461
ShobujDas <shobujd6.gmail.com>
2024-12-16 20:46:20 +0600

    some Change doing Home.jsx and Layout.jsx

--- client/src/axios.js
+++ client/src/axios.js
@@ -1,22 +1,22 @@
 
-import axios from "axios";
-
-const instance = axios.create({
-  baseURL: "https://deshpriomistibandare.onrender.com",
-  withCredentials: true, // Cookie পাঠানোর জন্য
-});
-
-export default instance;
-
-
 // import axios from "axios";
 
 // const instance = axios.create({
-//   baseURL: "http://localhost:5173",
+//   baseURL: "https://deshpriomistibandare.onrender.com",
 //   withCredentials: true, // Cookie পাঠানোর জন্য
 // });
 
 // export default instance;
+
+
+import axios from "axios";
+
+const instance = axios.create({
+  baseURL: "http://localhost:5173",
+  withCredentials: true, // Cookie পাঠানোর জন্য
+});
+
+export default instance;
 
 
 
